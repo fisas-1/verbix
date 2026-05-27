3d62bb1eff609fe126fb4e87471aef92dc008b4a
fisas-1 <marcal.fisas@plamarcell.net>
2026-05-27 20:20:13 +0200

    afegeixo codi AdSense

--- app/layout.tsx
+++ app/layout.tsx
@@ -32,7 +32,7 @@ export default function RootLayout({ children }: { children: React.ReactNode }) 
         {children}
         <Script
           async
-          src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-XXXXXXXXXXXXXXXX"
+          src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-4272225036233257"
           crossOrigin="anonymous"
           strategy="afterInteractive"
         />
