ac3e5131f2ccb08914ebc1e9004102de2a20da7b
fisas-1 <marcal.fisas@plamarcell.net>
2026-05-27 21:32:26 +0200

    afegeixo Google Analytics G-GL0H30FC92

--- app/layout.tsx
+++ app/layout.tsx
@@ -36,6 +36,18 @@ export default function RootLayout({ children }: { children: React.ReactNode }) 
           crossOrigin="anonymous"
           strategy="afterInteractive"
         />
+        <Script
+          src="https://www.googletagmanager.com/gtag/js?id=G-GL0H30FC92"
+          strategy="afterInteractive"
+        />
+        <Script id="google-analytics" strategy="afterInteractive">
+          {`
+            window.dataLayer = window.dataLayer || [];
+            function gtag(){dataLayer.push(arguments);}
+            gtag('js', new Date());
+            gtag('config', 'G-GL0H30FC92');
+          `}
+        </Script>
       </body>
     </html>
   );
